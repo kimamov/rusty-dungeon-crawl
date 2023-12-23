faa9348d17a24842dc97558618a076c4fac0d902
kantimam <kantemir.imam@gmail.com>
2023-12-23 21:09:12 +0100

    fixed camera

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+#![warn(clippy::pedantic)]
+
+mod camera;
 mod map;
 mod map_builder;
+mod player;
 
 mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub use crate::camera::*;
     pub use crate::map::*;
     pub use crate::map_builder::*;
+    pub use crate::player::*;
 }
 
 use prelude::*;
@@ -14,6 +22,7 @@ use prelude::*;
 struct State {
     map: Map,
     player: Player,
+    camera: Camera,
 }
 
 impl State {
@@ -23,57 +32,33 @@ impl State {
         Self {
             map: map_builder.map,
             player: Player::new(map_builder.player_start),
+            camera: Camera::new(map_builder.player_start),
         }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
         ctx.cls();
-        self.player.update(ctx, &self.map);
-        self.map.render(ctx);
-        self.player.render(ctx);
-    }
-}
-
-pub struct Player {
-    pub position: Point,
-}
-
-impl Player {
-    pub fn new(position: Point) -> Self {
-        Self { position }
-    }
-    pub fn render(&self, ctx: &mut BTerm) {
-        ctx.set(
-            self.position.x,
-            self.position.y,
-            WHITE,
-            BLACK,
-            to_cp437('@'),
-        )
-    }
-    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
-        if let Some(key) = ctx.key {
-            let delta = match key {
-                VirtualKeyCode::Left => Point::new(-1, 0),
-                VirtualKeyCode::Right => Point::new(1, 0),
-                VirtualKeyCode::Up => Point::new(0, -1),
-                VirtualKeyCode::Down => Point::new(0, 1),
-                _ => Point::zero(),
-            };
-            let new_position = self.position + delta;
-            if map.can_enter_tiles(new_position) {
-                self.position = new_position;
-            }
-        }
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.player.update(ctx, &self.map, &mut self.camera);
+        self.map.render(ctx, &self.camera);
+        self.player.render(ctx, &self.camera);
     }
 }
 
 fn main() -> BError {
-    let context = BTermBuilder::simple80x50()
-        .with_title("Rusty Crawl")
+    let context = BTermBuilder::new() // (1)
+        .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
+        .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT) // (2)
+        .with_tile_dimensions(32, 32) // (3)
+        .with_resource_path("resources/") // (4)
+        .with_font("dungeonfont.png", 32, 32) // (5)
+        .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (6)
+        .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png") // (7)
         .build()?;
 
     main_loop(context, State::new())
